1aabc57adf6f68fe77d45ae35ad6fc7b8bac13c1
Yazeed Sabri <ysabri@wisc.edu>
2017-10-27 17:08:37 -0500

    added comments and script to generate sql database # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	modified:   routes/index.js #	new file:   sql.sh #

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ function queryWord(callback) {
 	var num  = rand.integer(0,365713);
 
 	//let wordPromise = new Promise(function (fulfill, reject) {
+	/*query db for a word given a random id */
 		db.serialize(function() {
 			db.get("SELECT word as word FROM Words WHERE id=" + num, 
 				function (err, row) {
@@ -58,9 +59,13 @@ function initHtml(word, misses, callback) {
 		callback(result, misses);
 
 }
+
 /*create word html*/
 function createHtml(word, guessed, misses ,callback) {
+		//vars for the guesses and the missed letters
 		var result = '', misresult='';
+		/*compare the guesses array with the guessed letters,
+		* write it out if it is there, otherwise write spaces*/
 		for (var i = 0; i < word.length; i++){
 			var rend = '';
 			if(guessed[i] != undefined){
@@ -70,6 +75,7 @@ function createHtml(word, guessed, misses ,callback) {
 			result += '<div class="wordbox">\n\t<h3 class="letter">' + rend
 				+ '</h3>\n\t<hr class = "line">\n</div>\n';
 		}
+		//create the misses html
 		for(var i = 0; i < misses.length; i++){
 			if(i==misses.length-1){
 				misresult += '<div class="wordbox">\n\t<h3 class="letter">' 
@@ -87,6 +93,7 @@ function createHtml(word, guessed, misses ,callback) {
 /*Render all the html elements*/
 function renderHtml(req, res, callback){
 
+	//render html using session vars
 	createHtml(req.session.word, req.session.guessed, req.session.misses, 
 			function(wordHtml, missesHtml) {
 		res.render('index', { title: 'Hangman',
@@ -104,21 +111,30 @@ function renderHtml(req, res, callback){
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+	//user came back
 	if(req.session.seen){
 		console.log('Welcome back ' + req.session.word);
 		renderHtml(req, res, function() {});
 		return;
 	}
 
+	//new user here..
 	console.log('first time');
 
+	/*initialize session variables*/
 	req.session.seen = true;
+	//number of wrong guesses
 	req.session.guesses = 0;
+	//missed letters
 	req.session.misses = [''];
+	//guessed correct letters
 	req.session.guessed = [''];
+	//number of wins
 	req.session.wins = 0;
+	//number of loses
 	req.session.loses = 0;
 
+	/*get the word and initialize the session word*/
 	queryWord(function(result) {
 		req.session.word = result;
 		console.log(result);
@@ -131,17 +147,17 @@ router.get('/', function(req, res, next) {
 
 /*Picks a new word for the user*/
 router.get('/reset', function(req, res, next){
-
+	//new user, redirect to initialize them
 	if(req.session.seen === undefined){
     	console.log('redirecting...');
     	res.redirect('/');
     	return;
   	} 
-
+  	//reset the the session word vars
   	req.session.guesses = 0;
   	req.session.guessed = [''];
   	req.session.misses = [''];
-
+  	//get new word
   	queryWord(function(result) {
 		req.session.word = result;
 		console.log(result);
@@ -151,7 +167,7 @@ router.get('/reset', function(req, res, next){
 	});
 
 });
-
+/* Decide what to do with the guess and update variable sessions based on it */
 function guessChecker(body, req, callback) {
 
 	var letter = body.toUpperCase();
@@ -160,6 +176,7 @@ function guessChecker(body, req, callback) {
 	//guessed true a letter that has not been guessed before
 	if(req.session.guessed.indexOf(letter)<0 && 
 		req.session.word.indexOf(letter.toLowerCase()) > -1){
+		//add all occurrences of letter into guessed array
 		for(var j=0; j<req.session.word.length; j++){
 			if(req.session.word.charAt(j)==letter.toLowerCase()){
 				req.session.guessed[j]=letter;
@@ -193,28 +210,32 @@ function guessChecker(body, req, callback) {
 		req.session.guesses++;
 		req.session.misses += letter;
 	}
-//	console.log(req.session.guesses + ' guesses');
-//	console.log(req.session.misses + ' misses');
-//	console.log(req.session.guessed + ' guessed');
-//	console.log(req.session.wins + ' wins');
-//	console.log(req.session.loses + ' loses');
+/*	
+	console.log(req.session.guesses + ' guesses');
+	console.log(req.session.misses + ' misses');
+	console.log(req.session.guessed + ' guessed');
+	console.log(req.session.wins + ' wins');
+	console.log(req.session.loses + ' loses');
+*/
 	callback(guess);
 }
 
 /*recieve a guess*/
 router.post('/', function(req, res, next) {
 
+	//new user, redirect to init session
 	if(req.session.seen === undefined){
     	console.log('redirecting...');
     	res.redirect('/');
     	return;
   	} 
-  	
+
+  	//check for invalid guess values
   	if(regex.test(req.body.guess) || req.body.guess.length!=1){
   		console.log("&&&&&&&&&&");
   		res.sendStatus(400);
   	}
-
+  	//decide what to do with guess and send response
   	guessChecker(req.body.guess,req, function(guess){
   		if(guess){
   			queryWord(function(result) {
---
+++ sql.sh
@@ -0,0 +1,2 @@
+sqlite3 words < create.sql
+sqlite3 words < load.txt
