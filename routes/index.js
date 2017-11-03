0d229eb79eebb7f7eee3f8ff35e894158dc5270a
Yazeed Sabri <ysabri@wisc.edu>
2017-11-02 23:19:50 -0500

    count loses when reseting+fail safe db query+congrats/lose sentences
    So now the logic will count reseting as losing a point no matter what,
    that way poeple won't cheat.
    Also when losing/reseting it will display the word till a guess is made
    in the new automatically generated game.
    It will congrts the user when winning.
    Lastly, I added an array of 20 words as a fail safe for when the
    database takes too long to get back with a query, which when tested on
    my machine happened more often than I thought. I happens way more when
    the code runs in production given that I do not console log anything in
    production except which port the app is listing on, boosting the
    performance of the app a lot, What happens is that the async call
    doesn't return in time for it to be read. This is probably also due to other
    optimizaitons done in production.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 1 commit.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   routes/index.js
    #	modified:   views/index.hbs
    #

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,27 @@ var regex = new Regex('[A-Za-z]');
 file = './words';
 var db = new sqlite3.Database(file);
 
-debug = true;
+//words to pick from in case database takes too long to get back with a word.
+//This means, in the worse case, the probability of a number being picked twice
+//(I think) is 1 - (19/20)^n, n being how many times the user picks during the worse
+//case. This is usally not high, but its a proof of concept.
+var inCase = ['Earthbred', 'Upclimb', 'Euphonized', 'Cholelith', 'Retributive',
+	'Secreta', 'Utterable', 'Angulates', 'Skunkdom', 'Marooning',
+	'Releaser', 'Hymenophore', 'Disgorges', 'Podophyllous', 'Emporial',
+	'Shemaal', 'Inquisitors', 'Riflebird', 'Surfusion', 'Parilicium'
+];
+//setup debug flag
+dbg = true;
 if (process.env.NODE_ENV == 'production')
-	debug = false;
+	dbg = false;
 
 /*Query DB for word using the rand num*/
-function queryWord(callback) {
+function queryWord(oldword, callback) {
+
+	if (dbg) {
+		callback('testword');
+		return;
+	}
 
 	var num = rand.integer(0, 365713);
 
@@ -23,16 +38,21 @@ function queryWord(callback) {
 		db.get("SELECT word as word FROM Words WHERE id=" + num,
 			function(err, row) {
 				if (err) {
-					debug && console.log(err);
+					dbg && console.log(err);
 					//	reject(err);
 				} else {
 					//	fulfill(row.word);
-					if (row != undefined) {
-						callback(row.word);
-					} else {
-						callback(req.session.word);
+					try {
+						if (row != undefined) {
+							callback(row.word);
+						} else {
+							callback(oldWord);
+						}
+					} catch (e) {
+						console.log('in case happened');
+						i = rand.integer(0, 19);
+						callback(inCase[i]);
 					}
-
 				}
 			});
 	});
@@ -42,7 +62,7 @@ function queryWord(callback) {
 	//	callback(word);
 	//});
 
-	debug && console.log(num);
+	dbg && console.log(num);
 
 }
 
@@ -62,10 +82,13 @@ function initHtml(word, misses, callback) {
 }
 
 /*create word html*/
-function createHtml(word, guessed, misses, callback) {
+function createHtml(word, guessed, misses, status, oldWord, callback) {
 	//vars for the guesses and the missed letters
 	var result = '',
-		misresult = '';
+		misresult = '',
+		gameStat = '',
+		displayWord = '';
+
 	/*compare the guesses array with the guessed letters,
 	 * write it out if it is there, otherwise write spaces*/
 	for (var i = 0; i < word.length; i++) {
@@ -84,7 +107,24 @@ function createHtml(word, guessed, misses, callback) {
 			misresult += '<div class="wordbox">\n\t<h3 class="letter">' + misses[i].toUpperCase() + ',</h3>\n</div>\n';
 		}
 	}
-	callback(result, misresult);
+	//create lost or won html
+	var wid = '20px',
+		worl;
+	//player won
+	if (status == 1) {
+		wid = '330px';
+		worl = 'Congratulations you won!';
+	} //player lost
+	else if (status == -1) {
+		wid = '160px';
+		worl = 'The word was: ';
+		displayWord = '<h4 class="label" style="width: 45px;">' +
+			oldWord.charAt(0).toUpperCase() + oldWord.slice(1) + '</h4>';
+	}
+	if (worl)
+		gameStat = '<h4 class="label" style="width:' + wid + ';">' + worl + '</h4>';
+
+	callback(result, misresult, gameStat, displayWord);
 }
 
 /*Render all the html elements*/
@@ -92,7 +132,8 @@ function renderHtml(req, res, callback) {
 
 	//render html using session vars
 	createHtml(req.session.word, req.session.guessed, req.session.misses,
-		function(wordHtml, missesHtml) {
+		req.session.gameStatus, req.session.oldWord,
+		function(wordHtml, missesHtml, gameStatHtml, displayWord) {
 			res.render('index', {
 				title: 'Hangman',
 				image: '/images/hangState/Hangman-' +
@@ -100,9 +141,20 @@ function renderHtml(req, res, callback) {
 				word: wordHtml,
 				misses: missesHtml,
 				wins: req.session.wins,
-				loses: req.session.loses
+				loses: req.session.loses,
+				gameStatus: gameStatHtml,
+				notGuessed: displayWord
 			});
 		});
+
+	if (dbg) {
+		console.log(req.session.guesses + ' guesses');
+		console.log(req.session.misses + ' misses');
+		console.log(req.session.guessed + ' guessed');
+		console.log(req.session.wins + ' wins');
+		console.log(req.session.loses + ' loses');
+		console.log(req.session.gameStatus + ' gameStatus');
+	}
 
 	callback();
 }
@@ -112,13 +164,13 @@ router.get('/', function(req, res, next) {
 
 	//user came back
 	if (req.session.seen) {
-		debug && console.log('Welcome back ' + req.session.word);
+		dbg && console.log('Welcome back ' + req.session.word);
 		renderHtml(req, res, function() {});
 		return;
 	}
 
 	//new user here..
-	debug && console.log('first time');
+	dbg && console.log('first time');
 
 	/*initialize session variables*/
 	req.session.seen = true;
@@ -132,11 +184,15 @@ router.get('/', function(req, res, next) {
 	req.session.wins = 0;
 	//number of loses
 	req.session.loses = 0;
+	//lost = -1, won = 1, playing = 0, reseting = 2
+	req.session.gameStatus = 0;
+	//old word
+	req.session.oldWord = '';
 
 	/*get the word and initialize the session word*/
-	queryWord(function(result) {
+	queryWord(req.session.word, function(result) {
 		req.session.word = result;
-		debug && console.log(result);
+		dbg && console.log(result);
 		renderHtml(req, res, function() {
 
 		});
@@ -148,30 +204,40 @@ router.get('/', function(req, res, next) {
 router.get('/reset', function(req, res, next) {
 	//new user, redirect to initialize them
 	if (req.session.seen === undefined) {
-		debug && console.log('redirecting...');
+		dbg && console.log('redirecting...');
 		res.redirect('/');
 		return;
 	}
+	//made a guess, or just reseting normally
+	if (req.session.guesses > 0 || req.session.gameStatus == 0 ||
+		req.session.gameStatus == -1) {
+		dbg && console.log('in the loser is losing a point');
+		req.session.oldWord = req.session.word;
+		req.session.loses++;
+	}
 	//reset the the session word vars
+	req.session.gameStatus = -1;
 	req.session.guesses = 0;
 	req.session.guessed = [''];
 	req.session.misses = [''];
 	//get new word
-	queryWord(function(result) {
+	queryWord(req.session.word, function(result) {
 		req.session.word = result;
-		debug && console.log(result);
+		dbg && console.log(result);
 		renderHtml(req, res, function() {
 
 		});
 	});
 
 });
-/* Decide what to do with the guess and update variable sessions based on it */
+/* Decide what to do with the guess and update variabl esessions based on it */
 function guessChecker(body, req, callback) {
 
 	var letter = body.toUpperCase();
 	var guess = false;
-
+	if (req.session.guesses == 9 && req.session.gameStatus == -1) {
+		req.session.guesses = 0;
+	}
 	//guessed true a letter that has not been guessed before
 	if (req.session.guessed.indexOf(letter) < 0 &&
 		req.session.word.indexOf(letter.toLowerCase()) > -1) {
@@ -181,11 +247,13 @@ function guessChecker(body, req, callback) {
 				req.session.guessed[j] = letter;
 			}
 		}
+		req.session.gameStatus = 0;
 		//player won
 		if (req.session.guessed.join("").toLowerCase() == req.session.word) {
-			//debug && console.log('won');
+			dbg && console.log('won');
 			guess = true;
 			req.session.wins++;
+			req.session.gameStatus = 1;
 			req.session.guesses = 0;
 			req.session.guessed = [''];
 			req.session.misses = [''];
@@ -193,28 +261,22 @@ function guessChecker(body, req, callback) {
 		callback(guess);
 		return;
 	}
-
 	//player lost
 	if (req.session.guesses == 9) {
 		guess = true;
 		req.session.loses++;
-		req.session.guesses = 0;
+		req.session.gameStatus = -1;
+		req.session.oldWord = req.session.word;
+		req.session.guesses = 9;
 		req.session.guessed = [''];
 		req.session.misses = [''];
 		// has not been in the misses and guesses
 	} else if (req.session.misses.indexOf(letter) < 0 && req.session.guessed.indexOf(letter) < 0) {
+		req.session.gameStatus = 0;
 		req.session.guesses++;
 		req.session.misses += letter;
 	}
-	/*
-		if(debug){
-			console.log(req.session.guesses + ' guesses');
-			console.log(req.session.misses + ' misses');
-			console.log(req.session.guessed + ' guessed');
-			console.log(req.session.wins + ' wins');
-			console.log(req.session.loses + ' loses');
-		}
-	*/
+
 	callback(guess);
 }
 
@@ -223,22 +285,22 @@ router.post('/', function(req, res, next) {
 
 	//new user, redirect to init session
 	if (req.session.seen === undefined) {
-		debug && console.log('redirecting...');
+		dbg && console.log('redirecting...');
 		res.redirect('/');
 		return;
 	}
 
 	//check for invalid guess values
 	if (regex.test(req.body.guess) || req.body.guess.length != 1) {
-		debug && console.log("&&&&&&&&&&");
+		dbg && console.log("&&&&&&&&&&");
 		res.sendStatus(400);
 	}
 	//decide what to do with guess and send response
 	guessChecker(req.body.guess, req, function(guess) {
 		if (guess) {
-			queryWord(function(result) {
+			queryWord(req.session.word, function(result) {
 				req.session.word = result;
-				debug && console.log(result);
+				dbg && console.log(result);
 				renderHtml(req, res, function() {
 
 				});
@@ -250,7 +312,7 @@ router.post('/', function(req, res, next) {
 		}
 	});
 
-	debug && console.log(req.body.guess);
+	dbg && console.log(req.body.guess);
 
 });
 
